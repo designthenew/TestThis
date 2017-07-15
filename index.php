204384545ac5c808fc12a66a278dd4dcfbd33059
designthenew <designthenew.limited@gmail.com>
2017-07-15 17:06:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,3 @@
       <p>the content hges here</p>
     <div>
   </main>
-</body>
-                           
-  <footer>
-    <div class "thefooter">
-      <p>the footer content coges here</p>
-    </div>
-  </footer>
-
