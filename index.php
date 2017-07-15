2445f4a16208d8e0993c509dff9c73544b135cb1
designthenew <designthenew.limited@gmail.com>
2017-07-15 17:14:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-
+<?php echo file_get_contents("header.php"); ?>
   <main>
     <div class="thecontent">
       <p>the content hges here</p>
     <div>
   </main>
+  
+ <?php> echo file_get_contents("footer.php"); ?>
