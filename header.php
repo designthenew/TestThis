f9708ca827bbd98e8f55c83bae91edd96f4df464
designthenew <designthenew.limited@gmail.com>
2017-07-15 17:08:34 +0100

    Update header.php

--- header.php
+++ header.php
@@ -2,11 +2,9 @@
 <link rel="stylesheet" href="https://designthenew.github.io/TestThis/style.css">
 </head>
 
-
+<body>
   <header>
     <div class="thetitle">
         <h1>Git Test Web Site</h1>
       </div>
   </header>
-                
-<body>
