010f1e53498ac49eef710cf3190f7ad2efeaa840
designthenew <designthenew.limited@gmail.com>
2017-07-15 17:16:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php echo file_get_contents("header.php"); ?>
+<?php echo file_get_contents("https://designthenew.github.io/TestThis/header.php"); ?>
   <main>
     <div class="thecontent">
       <p>the content hges here</p>
     <div>
   </main>
   
- <?php> echo file_get_contents("footer.php"); ?>
+ <?php echo file_get_contents("https://designthenew.github.io/TestThis/footer.php"); ?>
