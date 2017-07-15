19894dd52f6c3ac7829c871a6c034b0009a04641
designthenew <designthenew.limited@gmail.com>
2017-07-15 17:05:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<head>
-<link rel="stylesheet" href="https://designthenew.github.io/TestThis/style.css">
-</head>
 
-
-  <header>
-    <div class="thetitle">
-        <h1>Git Test Web Site</h1>
-      </div>
-  </header>
-                
-<body>
   <main>
     <div class="thecontent">
       <p>the content hges here</p>
